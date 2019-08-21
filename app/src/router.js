254773260d91d9fed130e403702ed2cfc370ecd6
Fernando Rogelin <fernando.younner@gmail.com>
2019-08-21 15:42:10 -0300

    feat(routes): when user access any route that don't exist, redirect to /login

--- app/src/router.js
+++ app/src/router.js
@@ -1,22 +1,27 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Login from "./views/Login.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "login",
-      component: Login
+      path: '/login',
+      name: 'login',
+      component: () =>
+        import(/* webpackChunkName: "login" */ './views/Login.vue'),
     },
     {
-      path: "/register",
-      name: "register",
-      component: () => import(/* webpackChunkName: "register" */ "./views/Register.vue")
-    }
-  ]
+      path: '/register',
+      name: 'register',
+      component: () =>
+        import(/* webpackChunkName: "register" */ './views/Register.vue'),
+    },
+    {
+      path: '*',
+      redirect: '/login',
+    },
+  ],
 });
