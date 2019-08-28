d25d58b986e21148d9ac9b31164e4e4e844aa440
aislan <aislan.nadrowski@gmail.com>
2019-08-27 21:13:57 -0300

    Testing UserControler with /register GET request

--- api/src/main/java/br/com/squad1/api/usuario/controller/UserController.java
+++ api/src/main/java/br/com/squad1/api/usuario/controller/UserController.java
@@ -1,5 +1,23 @@
 package br.com.squad1.api.usuario.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
 public class UserController {
-  //Falta implementar.
+  
+
+//Falta implementar.
+
+  //Método de teste:
+  @GetMapping
+  public String testeRequisicao() {
+      System.out.println("Recebeu uma requisicao GET...");
+      return "Recebeu uma requisicao GET...";
+  }
+  
+  
+  
 }
