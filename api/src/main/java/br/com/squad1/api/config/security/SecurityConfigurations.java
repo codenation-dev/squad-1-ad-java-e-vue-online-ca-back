2e5b2bc11fba58263958cd60684c7a97d6f52d19
Aislan Nadrowski <aislan.nadrowski@gmail.com>
2019-09-02 20:40:15 -0300

    Correcting typo

--- api/src/main/java/br/com/squad1/api/config/security/SecurityConfigurations.java
+++ api/src/main/java/br/com/squad1/api/config/security/SecurityConfigurations.java
@@ -57,6 +57,6 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter  {
     //Configurações de recursos estáticos:
     @Override
     public void configure(WebSecurity web) throws Exception {
-      web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/wegjars/**", "/configuration/**", "/swagger-resources/**");
+      web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
     }
 }
