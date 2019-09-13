da4ee40db0d09379714171b2f4b1e2d411121710
davidalves1 <alves.david@outlook.com>
2019-09-12 23:02:15 -0300

    Atualizar o script do banco de dados

--- scripts/init.sql
+++ scripts/init.sql
@@ -8,34 +8,36 @@ GRANT ALL PRIVILEGES ON DATABASE central_erros_db TO cerr;
 \connect central_erros_db;
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id SERIAL NOT NULL, 
-    fullname VARCHAR(48) NOT NULL, 
-    email VARCHAR(40) NOT NULL UNIQUE, 
-    passwd VARCHAR(64) NOT NULL, 
-    created_at TIMESTAMP   DEFAULT NOW(),
+    id SERIAL NOT NULL, 
+    fullname VARCHAR(100) NOT NULL, 
+    email VARCHAR(100) NOT NULL UNIQUE, 
+    password VARCHAR(64) NOT NULL, 
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
     
-    PRIMARY KEY (user_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS levels (
-    level_id SERIAL NOT NULL,
+    id SERIAL NOT NULL,
     tag VARCHAR(20) NOT NULL,
 
-    PRIMARY KEY (level_id)
+    PRIMARY KEY (id)
 );
 
 INSERT INTO levels (tag)
     VALUES ('Error'), ('Debug'), ('Warning');
 	
 CREATE TABLE IF NOT EXISTS logs (
-    log_id SERIAL NOT NULL,
+    id SERIAL NOT NULL,
 	level_id INT NOT NULL,
-	description VARCHAR(512) NOT NULL,
-	origin VARCHAR(39) NOT NULL, -- size is enough to fit a ipv6 address
-	nr_events INT NOT NULL,
+	description TEXT NOT NULL,
+	origin VARCHAR(50) NOT NULL, -- size is enough to fit a ipv6 address
+	total_events INT NOT NULL,
 	details TEXT NOT NULL,
 	created_at TIMESTAMP DEFAULT NOW(),
+	updated_at TIMESTAMP DEFAULT NOW(),
 	
-    PRIMARY KEY (log_id),
-    FOREIGN KEY (level_id) REFERENCES levels(level_id)	
+    PRIMARY KEY (id),
+    FOREIGN KEY (level_id) REFERENCES levels(id)	
 );
