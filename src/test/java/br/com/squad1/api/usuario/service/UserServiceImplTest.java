5202439ea87e2439c7f05b0c1a72836b2288ea3f
davidalves1 <alves.david@outlook.com>
2019-10-06 02:38:58 -0300

    Remover pacote não utilizado

--- src/test/java/br/com/squad1/api/usuario/service/UserServiceImplTest.java
+++ src/test/java/br/com/squad1/api/usuario/service/UserServiceImplTest.java
@@ -2,7 +2,6 @@ package br.com.squad1.api.usuario.service;
 
 import br.com.squad1.api.usuario.model.User;
 import br.com.squad1.api.usuario.repository.UserRepository;
-import com.sun.javafx.scene.control.behavior.OptionalBoolean;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
